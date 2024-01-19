961ec72cfbdd2f6554485b30d8deccf5bf94249c
Debosmit Neogi <59002072+Debosmit-Neogi@users.noreply.github.com>
2024-01-18 20:37:23 -0500

    Add files via upload

---
+++ C-1_deb/0.py
@@ -0,0 +1,38 @@
+
+from flask import Flask, request, redirect
+import os
+import subprocess
+import sys
+import MySQLdb
+
+app = Flask(__name__)
+
+def get_db_connection():
+    db = MySQLdb.connect(host=os.environ.get('MYSQL_HOST', 'localhost'),
+                         user=os.environ.get('MYSQL_USER', 'root'),
+                         passwd=os.environ.get('MYSQL_PASSWORD', ''),
+                         db=os.environ.get('MYSQL_DATABASE', 'test'))
+    return db
+
+#remove the given email from all subscriptions in the database
+#and return the number of subscriptions removed
+def remove_email_from_all_subscriptions_return_count(email):
+     db = get_db_connection()
+     cursor = db.cursor()
+     cursor.execute('DELETE FROM subscriptions WHERE email=%s', (email,))
+     db.commit()
+     count = cursor.rowcount
+     cursor.close()
+     db.close()
+     return count
+
+#URL handler to remove a user from all subscriptions
+@app.route('/unsubscribe', methods=['GET']) 
+def unsubscribe():
+    email = request.args.get('email')
+    if email is None:
+        return 'No email provided', 400
+    count = remove_email_from_all_subscriptions_return_count(email)
+    return 'Removed from %d subscriptions' % count
+    
+
