7b9de7208ad30c18003d5dad8196c7d41f3ba34b
Debosmit Neogi <59002072+Debosmit-Neogi@users.noreply.github.com>
2024-01-18 21:37:57 -0500

    Add files via upload

---
+++ M-2_deb/0.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, redirect
+import os
+import subprocess
+import sys
+import MySQLdb
+
+__author__ = ""redactedAuthorFirstname redactedAuthorLastname (redactedAuthorFirstname.redactedAuthorLastname@nyu.edu)""
+__license__ = "MIT"
+
+app = Flask(__name__)
+
+def get_db_connection():
+    db = MySQLdb.connect(host="localhost",
+                         user="root",
+                         passwd="root",
+                         db="db")
+    return db
+
+#remove the given email from all subscriptions in the database
+#and return the number of subscriptions removed
+def remove_email_from_all_subscriptions_return_count(email):
+        db = get_db_connection()
+        cur = db.cursor()
+        cur.execute("select count(*) from subscriptions where email = %s", (email,))
+        count = cur.fetchone()[0]
+        cur.execute("delete from subscriptions where email = %s", (email,))
+        db.commit()
+        cur.close()
+        db.close()
+        return count
+
+#URL handler to remove a user from all subscriptions
+@app.route('/unsubscribe', methods=['GET'])
+def unsubscribe():
+    email = request.args.get('email', '')
+    count = remove_email_from_all_subscriptions_return_count(email)
+    return "Removed %d subscriptions" % count
+
---
+++ M-2_deb/8.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, redirect
+import os
+import subprocess
+import sys
+import MySQLdb
+
+__author__ = ""redactedAuthorFirstname redactedAuthorLastname (redactedAuthorFirstname.redactedAuthorLastname@nyu.edu)""
+__license__ = "MIT"
+
+app = Flask(__name__)
+
+def get_db_connection():
+    db = MySQLdb.connect(host="localhost",
+                         user="root",
+                         passwd="root",
+                         db="db")
+    return db
+
+#remove the given email from all subscriptions in the database
+#and return the number of subscriptions removed
+def remove_email_from_all_subscriptions_return_count(email):
+        db = get_db_connection()
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM subscriptions WHERE email='%s'" % email)
+        subscriptions = cursor.fetchall()
+        for subscription in subscriptions:
+            cursor.execute("DELETE FROM subscriptions WHERE id=%d" % subscription[0])
+        db.commit()
+        cursor.close()
+        db.close()
+        return len(subscriptions)
+
+#URL handler to remove a user from all subscriptions
+@app.route('/unsubscribe', methods=['GET'])
+def unsubscribe():
+    email = request.args.get('email', '')
+    count = remove_email_from_all_subscriptions_return_count(email)
+    return "Removed %d subscriptions" % count
+
